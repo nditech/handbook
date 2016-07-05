fc0e454c92ff22e5506b44a2f8196725feb6792e
Christopher Gervais <chris@ergonlogic.com>
2016-07-05 19:03:26 -0400

    Fix theme custom coloring.

--- themes/custom/handbook_theme/template.php
+++ themes/custom/handbook_theme/template.php
@@ -60,25 +60,3 @@ function handbook_process_block(&$vars) {
 }
 // */
 
-
-/**
- * Implements template_process_html().
- *
- * Override or insert variables into the page template for HTML output.
- */
-function handbook_process_html(&$variables) {
-  // Hook into color.module.
-  if (module_exists('color')) {
-    _color_html_alter($variables);
-  }
-}
-
-/*
- * Implements template_process_page().
- */
-function handbook_process_page(&$variables, $hook) {
-  // Hook into color.module.
-  if (module_exists('color')) {
-    _color_page_alter($variables);
-  }
-}
